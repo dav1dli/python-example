6a6b2d2de3a0df2d5d293fa57d09c750cd40a37e
Лидерман Вадим <vadim.liderman@gazprombank.ru>
2022-06-17 12:18:34 +0200

    Add health endpoint

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask
+from flask import jsonify
 app = Flask(__name__)
 
 @app.route('/')
 def hello_geek():
     return '<h1>Hello from Flask & Docker</h2>'
 
+@app.route('/health')
+def health():
+    """health route"""
+    state = {"status": "UP"}
+    return jsonify(state)
 
 if __name__ == "__main__":
     app.run(debug=True)
